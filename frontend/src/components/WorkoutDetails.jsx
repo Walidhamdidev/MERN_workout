3d5e6335e02f92f382bcc734ef80d6cda55e4f84
walid hamdi <walidhamdidev@gmail.com>
2023-04-08 13:56:18 +0100

    add delete functionality

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,6 +1,32 @@
-import React from "react";
+import { useState } from "react";
+import useWorkoutContext from "../hooks/useWorkoutContext";
 
 function WorkoutDetails({ workout }) {
+  const { dispatch } = useWorkoutContext();
+  const [loading, setLoading] = useState(false);
+
+  const handleDelete = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `${process.env.REACT_APP_API_ENDPOINT}/${workout._id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      setLoading(false);
+      if (response.ok) {
+        const json = await response.json();
+        dispatch({ type: "DELETE_WORKOUT", payload: json });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="workout">
       <span className="title">Title: </span>
@@ -10,6 +36,13 @@ function WorkoutDetails({ workout }) {
 
       <span className="reps">Reps: </span>
       {workout.reps}
+      <button
+        disabled={loading}
+        className="btn-form btn-delete"
+        onClick={handleDelete}
+      >
+        Delete
+      </button>
     </div>
   );
 }
